1ccef2645fcebe44653a7f0a7689c5713f5f8348
davidkonrad <davidkonrad@gmail.com>
2016-02-27 13:08:54 +0100

    updated schema

--- projectdata/dnaliv.sql
+++ projectdata/dnaliv.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 25, 2016 at 12:53 AM
+-- Generation Time: Feb 27, 2016 at 01:07 PM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.13
 
@@ -23,28 +23,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `booking`
+-- Table structure for table `fag`
 --
 
-CREATE TABLE IF NOT EXISTS `booking` (
-  `booking_id` int(11) NOT NULL AUTO_INCREMENT,
-  `institution` varchar(100) NOT NULL,
-  `date` date NOT NULL,
-  `time` time NOT NULL,
-  `klassetrin` varchar(20) NOT NULL,
-  `adresse` varchar(80) NOT NULL,
-  `postnr` varchar(8) NOT NULL,
-  `by` varchar(50) NOT NULL,
-  `kommune` varchar(50) NOT NULL,
-  `laerer_navn` varchar(40) NOT NULL,
-  `laerer_email` varchar(40) NOT NULL,
-  `learer_tlf` varchar(20) NOT NULL,
-  `fag` varchar(20) NOT NULL,
-  `elever_antal` int(11) NOT NULL,
-  `laerer_antal` int(11) NOT NULL,
-  `kommentar` text NOT NULL,
-  PRIMARY KEY (`booking_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `fag` (
+  `fag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fag_navn` varchar(100) NOT NULL,
+  PRIMARY KEY (`fag_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `fag`
+--
+
+INSERT INTO `fag` (`fag_id`, `fag_navn`) VALUES
+(1, 'Natur og teknik'),
+(2, 'Biologi'),
+(3, 'Geografi'),
+(4, 'Fysik/Kemi'),
+(5, 'Naturvidenskabeligt grundforløb'),
+(6, 'Naturgeografi'),
+(7, 'Bioteknologi'),
+(8, 'Andet (uddyb i kommentar)');
 
 -- --------------------------------------------------------
 
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `booking` (
 --
 
 CREATE TABLE IF NOT EXISTS `klasse` (
-  `klasse_id` int(11) NOT NULL,
+  `klasse_id` int(11) NOT NULL AUTO_INCREMENT,
   `projekt_id` int(11) NOT NULL,
   `institution` varchar(100) NOT NULL,
   `adresse` varchar(100) NOT NULL,
@@ -67,7 +67,36 @@ CREATE TABLE IF NOT EXISTS `klasse` (
   `antal_elever` int(11) NOT NULL,
   `antal_laerer` int(11) NOT NULL,
   PRIMARY KEY (`klasse_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `klasse`
+--
+
+INSERT INTO `klasse` (`klasse_id`, `projekt_id`, `institution`, `adresse`, `postnr`, `kommune`, `klassetrin`, `fag`, `laerer_navn`, `laerer_tlf`, `laerer_email`, `antal_elever`, `antal_laerer`) VALUES
+(1, 1, '234434', '', '', '', '', '', '', '', '', 0, 0),
+(2, 1, 'asdasdasd', '', '', '', '', '', '', '', '', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klassetrin`
+--
+
+CREATE TABLE IF NOT EXISTS `klassetrin` (
+  `klassetrin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `klassetrin_navn` varchar(50) NOT NULL,
+  PRIMARY KEY (`klassetrin_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `klassetrin`
+--
+
+INSERT INTO `klassetrin` (`klassetrin_id`, `klassetrin_navn`) VALUES
+(1, '1.g'),
+(2, '2.g'),
+(3, '3.g');
 
 -- --------------------------------------------------------
 
@@ -91,6 +120,8 @@ CREATE TABLE IF NOT EXISTS `projekt` (
   `projekt_id` int(11) NOT NULL AUTO_INCREMENT,
   `projekt_kode` varchar(20) NOT NULL,
   `timestamp_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `projekt_dato` date NOT NULL,
+  `projekt_tidspunkt` time NOT NULL DEFAULT '09:09:00',
   PRIMARY KEY (`projekt_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
@@ -98,10 +129,24 @@ CREATE TABLE IF NOT EXISTS `projekt` (
 -- Dumping data for table `projekt`
 --
 
-INSERT INTO `projekt` (`projekt_id`, `projekt_kode`, `timestamp_created`) VALUES
-(1, 'QWERTY', '2016-02-24 22:35:37'),
-(2, 'test', '2016-02-24 22:36:14'),
-(11, 'ÆØÅ', '2016-02-24 23:13:33');
+INSERT INTO `projekt` (`projekt_id`, `projekt_kode`, `timestamp_created`, `projekt_dato`, `projekt_tidspunkt`) VALUES
+(1, 'QWERTYaaa', '2016-02-26 20:28:46', '2016-05-17', '19:00:00'),
+(2, 'test', '2016-02-26 18:15:35', '2016-05-31', '00:00:00'),
+(11, 'ÆØÅ', '2016-02-26 17:56:59', '2015-12-27', '00:00:00'),
+(12, 'sdadad', '2016-02-27 00:56:38', '0000-00-00', '09:09:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projekt_taxon`
+--
+
+CREATE TABLE IF NOT EXISTS `projekt_taxon` (
+  `projekt_taxon_id` int(11) NOT NULL AUTO_INCREMENT,
+  `projekt_id` int(11) NOT NULL,
+  `taxon_id` int(11) NOT NULL,
+  PRIMARY KEY (`projekt_taxon_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -142,9 +187,12 @@ CREATE TABLE IF NOT EXISTS `sample_taxon` (
 --
 
 CREATE TABLE IF NOT EXISTS `taxon` (
-  `_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`_id`)
+  `taxon_id` int(11) NOT NULL AUTO_INCREMENT,
+  `taxon_navn` varchar(100) NOT NULL,
+  `taxon_navn_dk` varchar(100) DEFAULT NULL,
+  `taxon_artsgruppe` varchar(50) DEFAULT NULL,
+  `taxon_basisliste` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`taxon_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -159,24 +207,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `sample`
---
-ALTER TABLE `sample`
-  ADD CONSTRAINT `sample_ibfk_1` FOREIGN KEY (`locality_id`) REFERENCES `locality` (`_id`),
-  ADD CONSTRAINT `sample_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`_id`);
-
---
--- Constraints for table `sample_taxon`
---
-ALTER TABLE `sample_taxon`
-  ADD CONSTRAINT `sample_taxon_ibfk_1` FOREIGN KEY (`sample_id`) REFERENCES `sample` (`_id`),
-  ADD CONSTRAINT `sample_taxon_ibfk_2` FOREIGN KEY (`taxon_id`) REFERENCES `taxon` (`_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
