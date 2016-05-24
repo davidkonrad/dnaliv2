ed9cbc4299a5097a65d316596fc7723ad75e1635
davidkonrad <davidkonrad@gmail.com>
2016-05-24 06:24:50 +0200

    db dump

--- projectdata/dnaliv.sql
+++ projectdata/dnaliv.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 23, 2016 at 12:57 PM
+-- Generation Time: May 24, 2016 at 06:23 AM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.13
 
